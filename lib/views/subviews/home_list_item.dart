db67a159804c24247ad10743b4fce09824b626f1
abdelrahman mohamed <abdelrahman.mohamed@coformatique.com>
2023-10-30 00:22:52 +0200

    feat: add project

---
+++ lib/views/subviews/home_list_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CryptoListItem extends StatelessWidget {
+  const CryptoListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Row(
+
+      ),
+    );
+  }
+}
